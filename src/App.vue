755a366eef461ef1373627e7ef71ba04a9b7bbd0
Oumarjalloh <ojalloh38gmail.com>
2023-01-18 14:14:38 +0100

    :sparkles: Create Landing Page

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <section class="bg-orange-500 flex">
+    <div class="flex-[50%] pl-4">
+      <img class="w-24 pt-2" src="../src/assets/Logoiim.png" alt="">
+      <img class="inset-24 w-72 absolute" src="../src/assets/Capture.png" alt="">
+    </div>
+    <div class="flex-[50%] pt-16 pb-20">
+      <p class="text-3xl pb-8 font-bold text-white ">L’application de Networking <br>
+        pour les étudiants ...</p>
+      <div class="flex gap-x-5">
+        <button> <img class="w-28" src="../src/assets/Applelogo.jpg" alt=""></button>
+        <button> <img class="w-28" src="../src/assets/Googlelogo.jpg" alt=""></button>
+      </div>
+    </div>
+
+  </section>
+  <section class="flex">
+    <div class="flex-1 w-1/2">
+      <img class="w-24 absolute bottom-0 left-36" src="../src/assets/QRCode.jpg" alt="">
+    </div>
+    <div class="flex-1 w-1/2 pt-14">
+      <h1 class="border-b-4 border-b-orange-500 inline-block pr-3 mb-4 font-bold text-xl">Features</h1>
+      <div class="flex">
+        <div class="flex-1 w-1/2">
+          <p class="font-bold mb-1">Gestion d’évènements</p>
+          <p class="mb-3 text-sm">Créer, participer à des <br> événements </p>
+          <p class="font-bold mb-1">Atout 2</p>
+          <p class="text-sm">Lorem ipsum jjdfasjdjfla <br>
+            fsafdfasdfsa</p>
+        </div>
+        <div class="flex-1 w-1/2">
+          <p class="font-bold mb-1">Atout 2</p>
+          <p class="mb-3 text-sm">Lorem ipsum jjdfasjdjfla <br>
+            fsafdfasdfsa</p>
+          <p class="font-bold mb-1">Atout 2</p>
+          <p class="text-sm">Lorem ipsum jjdfasjdjfla <br>
+            fsafdfasdfsa</p>
+        </div>
+
+      </div>
+    </div>
+
+  </section>
+</template>
+
+<style>
+
+</style>
